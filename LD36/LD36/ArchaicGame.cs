5949e579efad8a95d9e0aa797c2d3d28b1104d9e
slyprid <aaronfoley80@gmail.com>
2016-08-27 04:58:16 -0400

    Cleanup

--- LD36/LD36/ArchaicGame.cs
+++ LD36/LD36/ArchaicGame.cs
@@ -3,9 +3,6 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-using Microsoft.Xna.Framework.Media;
-
-using System.Collections.Generic;
 using LD36.Managers;
 
 namespace LD36
