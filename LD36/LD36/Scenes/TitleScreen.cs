68db02c3811357ef4caf27a02f9cb0e7c2702fc6
Xenoputtss <xenoputtss@gmail.com>
2016-08-27 00:05:30 -0400

    hit "M" to hear the sound of an animal that starts with that letter.

--- LD36/LD36/Scenes/TitleScreen.cs
+++ LD36/LD36/Scenes/TitleScreen.cs
@@ -1,10 +1,10 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
 
 namespace LD36.Scenes
 {
-    public class TitleScreen
-        : Scene
+    public class TitleScreen : Scene
     {
         public const string Title = "Title";
 
@@ -12,11 +12,12 @@ namespace LD36.Scenes
         {
             AdventureGame.Graphics.GraphicsDevice.Clear(Color.Black);
         }
+
         public override void Update(GameTime gameTime)
         {
             if (Keyboard.GetState().IsKeyDown(Keys.M))
             {
-
+                MediaPlayer.Play(AdventureGame.sounds["CowMoo1"]);
             }
             base.Update(gameTime);
         }
